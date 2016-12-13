b79520bf6f9d546c3c0e8cb6e1caa24f1093fecc
Ralph Christian M. Sarthou <ralphsarthou1999@gmail.com>
2016-12-13 08:54:52 +0800

    Create pythagorean.c

---
+++ pythagorean.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+main(){
+	int a,b,c;
+	printf("Enter value for a : ");
+	scanf("%d", &a);
+	printf("Enter value for b: ");
+	scanf("%d", &b);
+	printf("%d",c=sqrt((a*a)+(b*b)));
+}
