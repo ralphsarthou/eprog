184d6df9d7808aed06a55a1fdb92af1e2f61f84e
Ralph Christian M. Sarthou <ralphsarthou1999@gmail.com>
2016-12-20 07:49:54 +0800

    Create if_play.c

---
+++ if_play.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+	int grade;
+	
+	printf("Number: ");
+	scanf("%d",&grade);
+	if(grade%3 ==0 ){
+		printf ("Ed");
+	}if(grade%5 ==0){
+		printf ("Pogi");
+	}if ((grade%3 ==0 ) && ( grade%5 ==0)){
+		printf("Ed Pogi");
+	}
+	
+}
