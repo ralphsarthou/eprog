c2fef6e90bc836a52779374422fec3270ccafe70
Ralph Christian M. Sarthou <ralphsarthou1999@gmail.com>
2016-12-20 08:20:50 +0800

    Update if_play.c

--- if_play.c
+++ if_play.c
@@ -5,8 +5,11 @@ int main (){
 	printf("Number: ");
 	scanf("%d",&grade);
 	if(grade%3 ==0 ){
-		printf ("Ed");
-	}if(grade%5 ==0){
 		printf ("Pogi");
+	}if(grade%5 ==0){
+		printf ("Ed");
+	}if((grade%3 ==0 && grade%5 ==0)){
+		printf("Ed Pogi ");
 	}
+	
 }
