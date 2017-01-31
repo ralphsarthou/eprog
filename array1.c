ad4a563e68fb3f7fc6a3edcf40adf2f6d3500b71
Ralph Christian M. Sarthou <ralphsarthou1999@gmail.com>
2017-01-30 16:04:07 -0800

    Update array1.c

--- array1.c
+++ array1.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 int main(){
-	int i, grades[3],ave;
+	int i, grades[3],sum=0;
+	float ave=0;
 	for(i=0; i<3; i++){
 		printf("Enter grade %d: ", i+1);
 		scanf("%d", &grades[i]);
-		ave+=grades[i]/3;
 	}
+	for(i=0; i<3; i++){
+		sum+=grades[i];
+	}
+	ave=sum/3;
+	printf("%f\n",ave);
 	if (ave>=75)
 		printf("Pass");
 	else
